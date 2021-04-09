9f548ec313d632068597e7ecd263f10224c94442
r0849268 <82242267+r0849268@users.noreply.github.com>
2021-04-10 00:06:27 +0200

    Add files via upload

---
+++ 2021_JS_PE_opdracht -Nick Markie r0849268/js/validatie.js
@@ -0,0 +1,30 @@
+let ErrorMessage = [];
+
+function checkEmptyFields(){
+
+    
+
+}
+
+function toonMeldingen(){
+
+
+}
+
+function emailValidatie(){
+
+}
+
+function postcodeValidatie(){
+
+}
+
+function validatForm(){
+
+    checkEmptyFields()
+    toonMeldingen()
+    emailValidatie()
+    postcodeValidatie
+
+}
+
